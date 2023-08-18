6e919878b95c90053958ad30ede11a3ad28c0cd3
jorge Lopez <jlopez@flexion.us>
2023-08-18 15:10:11 -0700

    GenerateJwtCommand.java, options for MainCli.java

--- app/src/main/java/jwt/utils/external/cli/MainCli.java
+++ app/src/main/java/jwt/utils/external/cli/MainCli.java
@@ -4,26 +4,21 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
-import java.util.Arrays;
 
+@Command(name = "ti", subcommands = GenerateJwtCommand.class, description = "A command-line tool")
 public class MainCli implements Runnable {
 
-    @Option(names = {"--privatekey"}, description = "Path to the private key file")
-    private String privateKeyPath;
-
-    @Option(names = {"--output"}, description = "Path to the output file")
-    private String outputPath;
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Display help information")
+    private boolean helpRequested;
 
     @Override
     public void run() {
-        System.out.println("This is the main command");
-        System.out.println("testing the parameters...");
-        System.out.println("Private key path: " + privateKeyPath);
-        System.out.println("Output path: " + outputPath);
+        if (helpRequested) {
+            CommandLine.usage(this, System.out);
+        }
     }
 
     public static void main(String[] args) {
-        Arrays.stream(args).forEach(System.out::println);
         int exitCode = new CommandLine(new MainCli()).execute(args);
         System.exit(exitCode);
     }
