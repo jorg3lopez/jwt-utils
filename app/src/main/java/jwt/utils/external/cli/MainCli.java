5f62d91845bd75b8b55e482426952c71e999c55f
jorge Lopez <jlopez@flexion.us>
2023-08-18 12:20:48 -0700

    added shadowJa and minor changes to build.gradl

--- app/src/main/java/jwt/utils/external/cli/MainCli.java
+++ app/src/main/java/jwt/utils/external/cli/MainCli.java
@@ -1,15 +1,33 @@
 package jwt.utils.external.cli;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
 import java.util.Arrays;
 
 public class MainCli implements Runnable {
 
-    public static void main(String[] args) {
-        Arrays.stream(args).forEach(System.out::println);
-    }
+    @Option(names = {"--privatekey"}, description = "Path to the private key file")
+    private String privateKeyPath;
+
+    @Option(names = {"--output"}, description = "Path to the output file")
+    private String outputPath;
 
     @Override
     public void run() {
-
+        System.out.println("This is the main command");
+        System.out.println("testing the parameters...");
+        System.out.println("Private key path: " + privateKeyPath);
+        System.out.println("Output path: " + outputPath);
     }
+
+    public static void main(String[] args) {
+        Arrays.stream(args).forEach(System.out::println);
+        // CommandLine.run(new MainCli(), args);
+        int exitCode = new CommandLine(new MainCli()).execute(args);
+        System.exit(exitCode);
+    }
+
+
 }
